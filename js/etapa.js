65f7d5d934fb0b53abb7ec9b381f29f62fb9bca6
Ricardo Moreira <calditos1988@outlook.com>
2020-09-20 21:24:10 -0300

    Criação da primeira função

---
+++ js/etapa.js
@@ -0,0 +1,53 @@
+let etapas = [
+      {
+   	      titulo: 'VEREADOR',
+   	      numeros: 5,
+   	      candidatos:[
+               {
+                 numero:38111,
+                 nome: 'Fulano de tal',
+                 partido: 'ABC',
+                 fotos: [
+                     {url:'caldito_vereador',legenda:'Vereador',small: false}
+                 ]
+                
+                },
+                {
+                 numero:77222,
+                 nome:'Beltrano da silva',
+                 partido: 'DEFG',
+                 fotos:[
+                    {url:'gildo_povo.jpg',legenda:'Vereador'}
+                 ]
+                
+                },
+            ]
+      },      
+      {
+        titulo:'PREFEITO',
+        numeros:2,
+        candidatos:[
+           {
+      	         numero:99,
+      	         nome:'Ciclano',
+      	         partido:'ABC',
+      	         vice:'Cic',
+      	         fotos:[
+                     {url:'miguelzinho.jpg',legenda:'Prefeito'},
+                     {url:'professor_fonseca.jpg',legenda:'Vice-Prefeito'}
+      	         ]
+           },
+           {
+                numero: 84,
+                nome:'Zulano',
+                partido:'QWERTI',
+                vice:'Zul',
+                fotos:[
+                     {url:'miguelzinho.jpg',legenda:'Prefeito'},
+                     {url:'professor_fonseca.jpg',legenda:'Vice-Prefeito'}
+                 ]
+            },
+        ]
+     }
+];
+
